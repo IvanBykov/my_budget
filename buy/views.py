b04c0fe8ba71f5d7fcf76725c7d1ee57451c5c8a
Ivan <bykoviv78@gmail.com>
2022-05-31 22:27:18 +0700

    редактирование всех моделей через классы

--- buy/views.py
+++ buy/views.py
@@ -24,17 +24,11 @@ class CreateProduct(CreateView):
     success_url = '/product'
 
 
-def update_product(request, id_product: int):
-    product = Product.objects.get(id=id_product)
-    if request.method == 'POST':
-        form = ProductForm(request.POST, instance=product)
-        if form.is_valid():
-            form.cleaned_data
-            form.save()
-            return HttpResponseRedirect(reverse('list-product'))
-    else:
-        form = ProductForm(instance=product)
-    return render(request, 'buy/product.html', context={'form': form})
+class UpdateProduct(UpdateView):
+    model = Product
+    form_class = ProductForm
+    template_name = 'buy/product.html'
+    success_url = '/product'
 
 
 class ListBuy(ListView):
@@ -49,17 +43,11 @@ class CreateBuy(CreateView):
     success_url = '/buy'
 
 
-def update_buy(request, id_buy: int):
-    buy = Buy.objects.get(id=id_buy)
-    if request.method == 'POST':
-        form = BuyForm(request.POST, instance=buy)
-        if form.is_valid():
-            form.cleaned_data
-            form.save()
-            return HttpResponseRedirect(reverse("list-buy"))
-    else:
-        form = BuyForm(instance=buy)
-    return render(request, 'buy/buy.html', context={'form': form})
+class UpdateBuy(UpdateView):
+    model = Buy
+    form_class = BuyForm
+    template_name = 'buy/buy.html'
+    success_url = '/buy'
 
 
 class ListBrand(ListView):
@@ -73,19 +61,11 @@ class CreateBrand(CreateView):
     template_name = 'buy/brand.html'
     success_url = '/brand'
 
-
-def update_brand(request, id_brand: int):
-    brand = Brand.objects.get(id=id_brand)
-    if request.method == 'POST':
-        form = BrandForm(request.POST, instance=brand)
-        if form.is_valid():
-            form.cleaned_data
-            form.save()
-            redirect_urls = reverse("list-brand")
-            return HttpResponseRedirect(redirect_urls)
-    else:
-        form = BrandForm(instance=brand)
-    return render(request, 'buy/brand.html', context={'form': form})
+class UpdateBrand(UpdateView):
+    model = Brand
+    form_class = BrandForm
+    template_name = 'buy/brand.html'
+    success_url = '/brand'
 
 
 class ListCategory(ListView):
@@ -100,17 +80,11 @@ class CreateCategory(CreateView):
     success_url = '/category'
 
 
-def update_category(request, id_category: int):
-    category = Category.objects.get(id=id_category)
-    if request.method == 'POST':
-        form = CategoryForm(request.POST, instance=category)
-        if form.is_valid():
-            form.cleaned_data
-            form.save()
-            return HttpResponseRedirect(reverse('list-category'))
-    else:
-        form = CategoryForm(instance=category)
-    return render(request, 'buy/category.html', context={'form': form})
+class UpdateCategory(UpdateView):
+    model = Category
+    form_class = CategoryForm
+    template_name = 'buy/category.html'
+    success_url = '/category'
 
 
 class ListUnit(ListView):
@@ -125,17 +99,11 @@ class CreateUnit(CreateView):
     success_url = '/unit'
 
 
-def update_unit(request, id_unit: int):
-    unit = Unit.objects.get(id=id_unit)
-    if request.method == 'POST':
-        form = UnitForm(request.POST, instance=unit)
-        if form.is_valid():
-            form.cleaned_data
-            form.save()
-            return HttpResponseRedirect(reverse('list-unit'))
-    else:
-        form = UnitForm(instance=unit)
-    return render(request, 'buy/unit.html', context={'form': form})
+class UpdateUnit(UpdateView):
+    model = Unit
+    form_class = UnitForm
+    template_name = 'buy/unit.html'
+    success_url = '/unit'
 
 
 class ListMagazine(ListView):
@@ -152,23 +120,10 @@ class UpdateMagazine(UpdateView):
 
 class DeleteMagazine(DeleteView):
     model = Magazine
-    #form_class = MagazineForm
+    # form_class = MagazineForm
     # context_object_name = 'form'
     template_name = 'buy/magazine.html'
     success_url = '/magazine'
-
-
-def update_magazine(request, id_magazine: int):
-    magazine = Magazine.objects.get(id=id_magazine)
-    if request.method == 'POST':
-        form = MagazineForm(request.POST, instance=magazine)
-        if form.is_valid():
-            # form.cleaned_data
-            form.save()
-            return HttpResponseRedirect(reverse('list-magazine'))
-    else:
-        form = MagazineForm(instance=magazine)
-    return render(request, 'buy/magazine.html', context={'form': form})
 
 
 def del_magazine(request, id_magazine: int):
