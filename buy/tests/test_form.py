ab58f9b9abca7aac36e0a05d0d6d42d6951ed61d
Ivan <bykoviv78@gmail.com>
2022-07-05 22:46:31 +0700

    добавлен еще тест для ProductForm

--- buy/tests/test_form.py
+++ buy/tests/test_form.py
@@ -1,6 +1,6 @@
 from django.urls import reverse
 
-from buy.models import Magazine
+from buy.models import Magazine, Brand, Product
 from buy.tests.test_models import Settings
 
 
@@ -17,5 +17,39 @@ class MagazineFormTest(Settings):
             data=form_data,
             follow=True
         )
-        print(Magazine.objects.count())
-        #41:22
+        self.assertEqual(2, Magazine.objects.count())
+        # 41:22
+        saved_magaz = Magazine.objects.last()
+        self.assertEqual('Тестовый магазин', saved_magaz.name)
+        self.assertEqual('тестовый адрес', saved_magaz.address)
+
+
+class BrandFormTest(Settings):
+    def test_valid_form(self):
+        form_data = {
+            'name': 'Тестовый брэнд'
+        }
+        response = self.client.post(
+            reverse('create-brand'),
+            data=form_data,
+            follow=True
+        )
+        saved_brand = Brand.objects.last()
+        self.assertEqual(2, Brand.objects.count())
+        self.assertEqual('Тестовый брэнд', saved_brand.name)
+
+class ProductFormTest(Settings):
+    def test_valid_form(self):
+        form_data = {
+            'name': 'Тестовый продукт'
+        }
+        #надо проверить категорию
+        response = self.client.post(
+            reverse('create-product'),
+            data=form_data,
+            follow=True
+        )
+        self.assertEqual(2, Product.objects.count())
+        saved_prod = Product.objects.last()
+        self.assertEqual('Тестовый продукт', saved_prod.name)
+
